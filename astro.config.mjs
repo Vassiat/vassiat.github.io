48630fb67b33467518545b0b6ce5bd2bbeebc98c
Adrian <atablante04@gmail.com>
2025-04-14 09:25:23 -0400

    Alias config fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss(), tsconfigPaths()]
   }
 });
